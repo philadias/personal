6339ba72623e14da04db53fe81ea4df689af5857
philadias <philadias@users.noreply.github.com>
2021-01-01 18:43:25 +0000

    deploy: b5a05694332a7398e95f8ea0639855c41adec169

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,10 +1,4 @@
 var store = [{
-        "title": "An Example Markdown Post",
-        "excerpt":"Example Markdown Post Basic setup Jekyll requires blog post files to be named according to the following format: YEAR-MONTH-DAY-filename.md Where YEAR is a four-digit number, MONTH and DAY are both two-digit numbers, and filename is whatever file name you choose, to remind yourself what this post is about. .md is...","categories": ["markdown"],
-        "tags": [],
-        "url": "https://philadias.github.io/personal/markdown/test-markdown-post/",
-        "teaser": null
-      },{
         "title": "Fastpages Notebook Blog Post",
         "excerpt":"About This notebook is a demonstration of some of capabilities of fastpages with notebooks. With fastpages you can save your jupyter notebooks into the _notebooks folder at the root of your repository, and they will be automatically be converted to Jekyll compliant blog posts! Front Matter The first cell in...","categories": ["jupyter"],
         "tags": [],
